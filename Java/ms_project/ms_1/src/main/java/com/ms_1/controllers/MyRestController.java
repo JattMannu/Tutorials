76c067a4aecc9f710006b27662c247673b2249d6
Manpreet Singh <manpreet.singh@compareasiagroup.com>
2019-10-23 18:04:04 +0800

    Added Updates and Content type
    Signed-off-by: Manpreet Singh <manpreet.singh@compareasiagroup.com>

--- Java/ms_project/ms_1/src/main/java/com/ms_1/controllers/MyRestController.java
+++ Java/ms_project/ms_1/src/main/java/com/ms_1/controllers/MyRestController.java
@@ -1,6 +1,7 @@
 package com.ms_1.controllers;
 
 import com.ms_1.dto.Baby;
+import com.ms_1.dto.Boolean;
 import com.ms_1.dto.Car;
 import com.ms_1.dto.SeralizedCar;
 import com.ms_1.dto.Toy;
@@ -9,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +18,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class MyRestController {
@@ -36,8 +39,8 @@ public class MyRestController {
     public List getCars() {
         List cars = new LinkedList<Car>();
 
-        cars.add(new Car("Ford" , "T410" , 60, 6.4 , 'T' , 7.6f , 4.3));
-        cars.add(new Car("Ford" , "T415" , 67, 3.2 , 'T' , 5.6f , 7.3));
+        cars.add(new Car("Ford", "T410", 60, 6.4, 'T', 7.6f, 4.3));
+        cars.add(new Car("Ford", "T415", 67, 3.2, 'T', 5.6f, 7.3));
         return cars;
         // Output: [
         // {"name":"Ford","model":"T410","age":60,"tireWear":6.4,"type":"T","engineWear":7.6,"totalWear":4.3},
@@ -47,7 +50,7 @@ public class MyRestController {
 
     @RequestMapping(value = "/baby", method = RequestMethod.GET)
     public Baby getObjInsideObj() {
-        return new Baby("Jonny",3, new Toy("Teddy Bear"));
+        return new Baby("Jonny", 3, new Toy("Teddy Bear"));
         // Output: {"name":"Jonny","boxNumber":3,"toy":{"name":"Teddy Bear"}}
     }
 
@@ -63,12 +66,70 @@ public class MyRestController {
         StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
     }
 
-    //copy code
-    @RequestMapping(value = "/getImage1", method = RequestMethod.GET,
-            produces = MediaType.IMAGE_JPEG_VALUE)
-    public void getImage1(HttpServletResponse response) throws IOException {
+    @RequestMapping(value = "/getAudio.mp3", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public void getAudio(HttpServletResponse response) throws IOException {
 
-        SeralizedCar seralizedCar = new SeralizedCar();
-        StreamUtils.(seralizedCar, response.getOutputStream());
+        var mp3 = new ClassPathResource("audio/s1.mp3");
+
+        response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+        StreamUtils.copy(mp3.getInputStream(), response.getOutputStream());
+        //This will result in download of the file
+        // .* (binary stream, don't know the type of download file)	Application/octet-stream
+        // https://tool.oschina.net/commons
     }
+
+    @RequestMapping(value = "/playAudio", method = RequestMethod.GET,
+            produces = "Audio/mp3")
+    public void playAudio(HttpServletResponse response) throws IOException {
+
+        var mp3 = new ClassPathResource("audio/s1.mp3");
+
+        response.setContentType("Audio/mp3");
+        StreamUtils.copy(mp3.getInputStream(), response.getOutputStream());
+        //This will play the audio on the webpage
+
+        // https://tool.oschina.net/commons
+
+    }
+
+    @RequestMapping(value = "/terminal", method = RequestMethod.GET)
+    public Boolean terminal(HttpServletResponse response) {
+        Process proc = null;
+        try {
+            String[] cmdss = {"gnome-terminal"};
+
+
+            proc = Runtime.getRuntime().exec(cmdss);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Boolean(true);
+    }
+
+
+    @RequestMapping(value = "/action", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public  void action(@RequestParam("cmd") String cmd, HttpServletResponse response) {
+        Process proc = null;
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        try {
+            String cmdss[] = {"/bin/bash", "-c", cmd};
+            proc = Runtime.getRuntime().exec(cmdss);
+
+            proc.waitFor(5000, TimeUnit.MILLISECONDS);
+            StreamUtils.copy(proc.getInputStream(), response.getOutputStream());
+            //var mp3 = new ClassPathResource("audio/s1.mp3");
+
+
+            //StreamUtils.copy(mp3.getInputStream(), response.getOutputStream());
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
