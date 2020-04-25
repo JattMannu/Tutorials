f27e4886eb0f0ddbeba492108584067688bd688e
JattMannu <manpreet_firewall@yahoo.com.sg>
2020-04-25 17:54:20 +0800

    1. Using YAML files - Microservice configuration with Spring Boot [07]

---
+++ Java/spring-rest/Spring Boot Microservices Level 3/spring-boot-config-yaml/src/main/java/com/example/demo/DbSetting.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration // Added to ensure this is a bean. So can autowire
+@ConfigurationProperties("db")
+public class DbSetting {
+
+    private String connectionString;
+    private String host;
+    private int port;
+
+    public String getConnectionString() {
+        return connectionString;
+    }
+
+    public void setConnectionString(String connectionString) {
+        this.connectionString = connectionString;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
---
+++ Java/spring-rest/Spring Boot Microservices Level 3/spring-boot-config-yaml/src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,97 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class GreetingController {
+
+    //Value injection
+    @Value("${my.greeting}")
+    private String greetingMessage;
+
+    @Value("${app.description}")
+    private String appDescription;
+
+
+    // 1
+    @Value("This is a static Message")
+    private String staticMsg;
+
+    // 2
+    @Value("${my.greeting}")
+    private String fromApplicationProperties;
+
+    // 3 : Default Value
+    @Value("${my.greeting_missing: This is a default Value}")
+    private String fromDefaultValue;
+
+
+    // 4 :  List of values
+    @Value("${my.list.values}")
+    private List<String> myValueList;
+
+    // 5 :  Key Values pair
+    @Value("#{${my.keyValue}}")
+    private Map<String,String> map;
+
+
+    // curl localhost:8080/greeting
+    @GetMapping("/greeting")
+    public String greeting() {
+        return greetingMessage;
+    }
+
+    // curl localhost:8080/app-desc
+    @GetMapping("/app-desc")
+    public String appDesc() {
+        return appDescription;
+    }
+
+
+    // curl localhost:8080/printdefault
+    @GetMapping("/printdefault")
+    public String printdefault() {
+        return fromDefaultValue;
+    }
+
+    // curl localhost:8080/printList
+    @GetMapping("/printList")
+    public String printList() {
+        return "List: "+ myValueList;
+    }
+
+    // curl localhost:8080/printMap
+    @GetMapping("/printMap")
+    public String printMap() {
+        return "Map: "+ this.map;
+    }
+
+
+
+
+    // Lesson 6
+    @Autowired
+    private DbSetting dbSetting;
+
+    // curl localhost:8080/db-setting
+    @GetMapping("/db-setting")
+    public String dbSetting() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("dbSetting connection String : "+ dbSetting.getConnectionString());
+        builder.append("\n");
+        builder.append("dbSetting host: "+ dbSetting.getHost());
+        builder.append("\n");
+        builder.append("dbSetting port: "+ dbSetting.getPort());
+        return builder.toString();
+    }
+
+
+
+}
+
